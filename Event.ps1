4f38117d762de3e48ee7ca008d40cfd9cdbad451
Sh@d0w <32146568+ps81frt@users.noreply.github.com>
2025-05-22 03:10:49 +0200

    Update Event.ps1
    config sys

--- Event.ps1
+++ Event.ps1
@@ -2,12 +2,20 @@ New-Item -Path "$env:userprofile\Desktop\" -Name "Diag" -ItemType Directory;
 New-Item -Path "$env:userprofile\Desktop\Diag\" -Name "Application" -ItemType Directory;
 New-Item -Path "$env:userprofile\Desktop\Diag\" -Name "Systeme" -ItemType Directory;
 
+# Configguration Système:
+
+&{gwmi Win32_OperatingSystem | Select Caption, BuildNumber, Version, SystemDirectory |fl ; gwmi -Class Win32_BaseBoard | Format-List Product,Manufacturer, SerialNumber, Version ; gwmi -Class Win32_BIOS | Select-Object BIOSVersion,Manufacturer,SMBIOSBIOSVersion | fl  ; Get-CimInstance Win32_Processor | fl ; gwmi -ClassName win32_VideoController | Select-Object Description,DriverVersion,AdapterCompatibility,InfFilename,VideoModeDescription  | Format-List ; gwmi Win32_PhysicalMemory | Format-Table Manufacturer, SerialNumber, BankLabel, DeviceLocator, @{n="Size (GB)"; e={($_.Capacity/1GB)}; align="center"}, @{n="ClockSpeed (MHz)"; e={($_.ConfiguredClockSpeed)}; align="center"} -Auto ; Get-PhysicalDisk ; wmic /namespace:\\root\wmi path MSStorageDriver_FailurePredictStatus ; wmic startup get caption,command ; Get-WmiObject win32_service | where { $_.Caption -notmatch "Microsoft" -and $_.PathName -notmatch "Windows" } | Select-Object Name,Startmode,State,Status,ExitCode,ProcessId,DisplayName | Sort-Object -Property @{Expression = "Name" } |ft } | Out-File -Width 4096  $env:USERPROFILE\desktop\Diag\CONFIG_System.txt;
+
 # Apllication:
 
 Get-WinEvent -LogName Application -MaxEvents 1000 -Verbose | Where { $_.ID -eq 1000 } | Format-List  TimeCreated, AppName,  ProviderName, Id, Message | Out-File $env:userprofile\Desktop\Diag\Application\apli_1000.txt;
 Get-WinEvent -LogName Application -MaxEvents 1000 -Verbose | Where { $_.ID -eq 1001 } | Format-List  TimeCreated, AppName,  ProviderName, Id, Message | Out-File $env:userprofile\Desktop\Diag\Application\apli_1001.txt;
 Get-WinEvent -LogName Application -MaxEvents 1000 -Verbose | Where { $_.ID -eq 1005 } | Format-List  TimeCreated, AppName,  ProviderName, Id, Message | Out-File $env:userprofile\Desktop\Diag\Application\apli_1005.txt;
 Get-WinEvent -LogName Application -MaxEvents 1000 -Verbose | Where { $_.ID -eq 1026 } | Format-List  TimeCreated, AppName,  ProviderName, Id, Message | Out-File $env:userprofile\Desktop\Diag\Application\apli_1026.txt;
+
+# Event Apllications installer:
+
+Get-WinEvent -FilterHashtable @{ LogName = "Application"; ID = 1033 } | Select timecreated,level,message | Select -First 140 | Format-List | Out-File $env:userprofile\Desktop\Diag\Application\LogAppliInstaller_1033.txt;
 
 # Système:
 
