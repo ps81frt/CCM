a17a0198c1b70d574248e3e6f1694b4c1cad334a
Sh@d0w <32146568+ps81frt@users.noreply.github.com>
2025-05-22 02:59:55 +0200

    Update Event.ps1
    system

--- Event.ps1
+++ Event.ps1
@@ -1,5 +1,6 @@
 New-Item -Path "$env:userprofile\Desktop\" -Name "Diag" -ItemType Directory
 New-Item -Path "$env:userprofile\Desktop\Diag\" -Name "Application" -ItemType Directory
+New-Item -Path "$env:userprofile\Desktop\Diag\" -Name "Systeme" -ItemType Directory
 
 # Apllication:
 
@@ -9,3 +10,7 @@ Get-WinEvent -LogName Application -MaxEvents 1000 -Verbose | Where { $_.ID -eq 1
 Get-WinEvent -LogName Application -MaxEvents 1000 -Verbose | Where { $_.ID -eq 1026 } | Format-List  TimeCreated, AppName,  ProviderName, Id, Message | Out-File $env:userprofile\Desktop\Diag\Application\apli_1026.txt
 
 # Système:
+
+Write-Host "100 dernier evenement du journal system" ; Get-WinEvent -LogName System  -MaxEvents 100 | Select-Object TimeCreated,UserId,ContainerLog,ID,Level,Message,ProviderName,MachineName,TaskDisplayName,ProcessId,RecordId,Version,Task,Keywords | fl| fl | Out-File -FilePath $env:USERPROFILE\Desktop\Diag\Systeme\
+eventsystem2.txt;
+Get-EventLog -LogName System  -After (Get-Date).AddDays(-4) -EntryType Error, Warning | Select-Object -First 100 | fl | Out-File -FilePath $env:USERPROFILE\Desktop\Diag\Systeme\eventsystem.txt
