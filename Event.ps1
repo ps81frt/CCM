163ba92a825632a1206e6d4b8dd8a78161854e6c
Sh@d0w <32146568+ps81frt@users.noreply.github.com>
2025-05-22 03:00:20 +0200

    Update Event.ps1

--- Event.ps1
+++ Event.ps1
@@ -1,13 +1,13 @@
-New-Item -Path "$env:userprofile\Desktop\" -Name "Diag" -ItemType Directory
-New-Item -Path "$env:userprofile\Desktop\Diag\" -Name "Application" -ItemType Directory
-New-Item -Path "$env:userprofile\Desktop\Diag\" -Name "Systeme" -ItemType Directory
+New-Item -Path "$env:userprofile\Desktop\" -Name "Diag" -ItemType Directory;
+New-Item -Path "$env:userprofile\Desktop\Diag\" -Name "Application" -ItemType Directory;
+New-Item -Path "$env:userprofile\Desktop\Diag\" -Name "Systeme" -ItemType Directory;
 
 # Apllication:
 
-Get-WinEvent -LogName Application -MaxEvents 1000 -Verbose | Where { $_.ID -eq 1000 } | Format-List  TimeCreated, AppName,  ProviderName, Id, Message | Out-File $env:userprofile\Desktop\Diag\Application\apli_1000.txt
-Get-WinEvent -LogName Application -MaxEvents 1000 -Verbose | Where { $_.ID -eq 1001 } | Format-List  TimeCreated, AppName,  ProviderName, Id, Message | Out-File $env:userprofile\Desktop\Diag\Application\apli_1001.txt
-Get-WinEvent -LogName Application -MaxEvents 1000 -Verbose | Where { $_.ID -eq 1005 } | Format-List  TimeCreated, AppName,  ProviderName, Id, Message | Out-File $env:userprofile\Desktop\Diag\Application\apli_1005.txt
-Get-WinEvent -LogName Application -MaxEvents 1000 -Verbose | Where { $_.ID -eq 1026 } | Format-List  TimeCreated, AppName,  ProviderName, Id, Message | Out-File $env:userprofile\Desktop\Diag\Application\apli_1026.txt
+Get-WinEvent -LogName Application -MaxEvents 1000 -Verbose | Where { $_.ID -eq 1000 } | Format-List  TimeCreated, AppName,  ProviderName, Id, Message | Out-File $env:userprofile\Desktop\Diag\Application\apli_1000.txt;
+Get-WinEvent -LogName Application -MaxEvents 1000 -Verbose | Where { $_.ID -eq 1001 } | Format-List  TimeCreated, AppName,  ProviderName, Id, Message | Out-File $env:userprofile\Desktop\Diag\Application\apli_1001.txt;
+Get-WinEvent -LogName Application -MaxEvents 1000 -Verbose | Where { $_.ID -eq 1005 } | Format-List  TimeCreated, AppName,  ProviderName, Id, Message | Out-File $env:userprofile\Desktop\Diag\Application\apli_1005.txt;
+Get-WinEvent -LogName Application -MaxEvents 1000 -Verbose | Where { $_.ID -eq 1026 } | Format-List  TimeCreated, AppName,  ProviderName, Id, Message | Out-File $env:userprofile\Desktop\Diag\Application\apli_1026.txt;
 
 # Système:
 
